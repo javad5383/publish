6bcd3eb195fe6012749443ff5bff6c3c995f8e35
javad <javad.mohammadi5383@gmail.com>
2021-10-13 19:42:54 +0330

    first commit2

---
+++ wwwroot/assets/js/MyScriptjs.js
@@ -0,0 +1,23 @@
+﻿
+$(document).ready(function () {
+
+    $("#srchbox").autocomplete({
+        minLength: 3,
+        source: function (request, response) {
+            var search = $("#srchbox").val();
+            $.getJSON("/productsMain/SearchBox/?search=" + search,
+                function (data) {
+                    response(data);
+                });
+        }
+        ,
+        select: function (a, b) {
+
+            $("#SelectedId").val(b.item.data);
+            $("#srchfrm").submit();
+        }
+
+    });
+
+   
+});
